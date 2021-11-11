5b10b6e312aba96dbbc019f9644434e3b1b56666
R4FFF <69420740+R4FFF@users.noreply.github.com>
2021-11-11 14:59:41 +0000

    latest commit

--- src/components/contact-form/ContactForm.js
+++ src/components/contact-form/ContactForm.js
@@ -15,11 +15,11 @@ function ContactForm() {
                     <div className="contacts-form"><LocalPhoneIcon fontSize="medium" /><p>+33 0673346890</p></div>
                     <div className="contacts-form"><EmailIcon fontSize="medium" /><p>pascal.paire@orange.fr</p></div>
                 </div>
-                <form action="https://formsubmit.co/xoomegaredox@gmail.com" method="POST">
+                <form action="https://formsubmit.co/e8d4183aebb9cecb5af6933b481a6d04" method="POST">
                     <input type="text" name="_honey" style={{display: "none"}}/>
                     <input type="hidden" name="_captcha" value="false" />
-                    <input type="hidden" name="_next" value="http://localhost:3000/#/success" />
-                    <input type="hidden" name="_autoresponse" value="Thank you we have received you message" />
+                    <input type="hidden" name="_next" value="http://duplexvalthorens.com/#/success" />
+                    <input type="hidden" name="_template" value="box" />
                     <div className="form-group">
                         <input type="text" name="Name" placeholder={t("contacts.full_name")} required/>
                         <input type="email" name="Email" placeholder={t("contacts.email")} required/>
--- src/i18n.js
+++ src/i18n.js
@@ -5,6 +5,8 @@ import { initReactI18next } from "react-i18next"
 import translationEN from "../src/locales/en/translationEN.json"
 import translationFR from "../src/locales/fr/translationFR.json"
 
+const languages = ["en", "fr"];
+
 const resources = {
     fr: {
       translation: translationFR
@@ -15,17 +17,20 @@ const resources = {
   };
 
 i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
-    resources,
+    resources: resources,
+    whitelist: languages,
     lng: "fr",
     fallbackLng: "en",
     debug: true,
     detection: {
-        order: ["queryString", "cookie"],
+        order: ["navigator", "queryString", "cookie"],
         cache: ["cookie"]
     },
     interpolation: {
         escapeValue: false
-    }
+    },
 })
 
 export default i18n
+
+// lng: "fr",
